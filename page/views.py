b112f28d3881e38cb99e97c045e1096980ace976
Philipp Giese <giese.philipp+git@gmail.com>
2012-12-28 21:24:37 +0100

    added pagination to index page

--- page/views.py
+++ page/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from androrm.downloads.models import Release
 from androrm.page.models import Entry
@@ -9,6 +10,17 @@ def index(request):
     release = Release.objects.all().order_by("-version")[0:1]
 
     entries = Entry.objects.filter(published=True).order_by("-date")
+    paginator = Paginator(entries, 10)
+
+    page = request.GET.get("page")
+
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+
 
     if release:
         version = release[0].version
